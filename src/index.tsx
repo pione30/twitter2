ae0121c5a0016ad8863f07fec246c1da4a6a8334
pione30 <g.pionelish30@gmail.com>
2020-09-21 18:15:21 +0900

    use the getters instead of raw process.env values

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 
 import { App } from "./components/App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { getAuth0Domain, getAuth0ClientId } from "@/environmentVariables";
 
 ReactDOM.render(
   <Auth0Provider
-    domain={process.env.AUTH0_DOMAIN as string}
-    clientId={process.env.AUTH0_CLIENT_ID as string}
+    domain={getAuth0Domain()}
+    clientId={getAuth0ClientId()}
     redirectUri={window.location.origin}
   >
     <App></App>
