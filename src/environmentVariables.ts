d3cc4abe5f010d070254f11a821a9372e5775221
pione30 <g.pionelish30@gmail.com>
2020-09-21 17:54:46 +0900

    add module which exports getters of environment variables

---
+++ src/environmentVariables.ts
@@ -0,0 +1,15 @@
+export const getAuth0Domain = (): string => {
+  if (process.env.AUTH0_DOMAIN) {
+    return process.env.AUTH0_DOMAIN;
+  } else {
+    throw new Error("process.env.AUTH0_DOMAIN is not defined");
+  }
+};
+
+export const getAuth0ClientId = (): string => {
+  if (process.env.AUTH0_CLIENT_ID) {
+    return process.env.AUTH0_CLIENT_ID;
+  } else {
+    throw new Error("process.env.AUTH0_CLIENT_ID is not defined");
+  }
+};
