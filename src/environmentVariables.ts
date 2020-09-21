8f16aa43bb9aebdfc98ebd3f0fe5c45446440044
pione30 <g.pionelish30@gmail.com>
2020-09-21 17:20:56 +0900

    add audience

--- src/environmentVariables.ts
+++ src/environmentVariables.ts
@@ -13,3 +13,11 @@ export const getAuth0ClientId = (): string => {
     throw new Error("process.env.AUTH0_CLIENT_ID is not defined");
   }
 };
+
+export const getAuth0Audience = (): string => {
+  if (process.env.AUTH0_AUDIENCE) {
+    return process.env.AUTH0_AUDIENCE;
+  } else {
+    throw new Error("process.env.AUTH0_AUDIENCE is not defined");
+  }
+};
