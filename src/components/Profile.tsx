0eb6a8851351dde66d57bc347d5a84bc7d48aea2
pione30 <g.pionelish30@gmail.com>
2020-09-21 21:18:41 +0900

    update Profile.tsx to call the API

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,45 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useApi } from "@/hooks/useApi";
+import type { UseApiOptions } from "@/hooks/useApi"; // eslint-disable-line no-unused-vars
+import { getAuth0Audience } from "@/environmentVariables";
+import LoginButton from "./LoginButton";
 
 const Profile = () => {
-  const { user, isAuthenticated } = useAuth0();
+  const apiOpts: UseApiOptions = {
+    audience: getAuth0Audience(),
+    scope: "read:current_user",
+  };
 
+  const { user, isAuthenticated, getAccessTokenWithPopup } = useAuth0();
+  const { data: myPosts, loading, error, refresh } = useApi(
+    `${getAuth0Audience}/posts/own`,
+    apiOpts
+  );
+
+  const getTokenAndTryAgain = async () => {
+    await getAccessTokenWithPopup(apiOpts);
+    refresh();
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    if (error.error === "login_required") {
+      return <LoginButton></LoginButton>;
+    }
+    if (error.error === "consent_required") {
+      return (
+        <button onClick={getTokenAndTryAgain}>
+          Consent to reading current user
+        </button>
+      );
+    }
+    return <div>Oops {error.message}</div>;
+  }
+
+  // TODO: display myPosts
   return (
     <>
       {isAuthenticated && (
