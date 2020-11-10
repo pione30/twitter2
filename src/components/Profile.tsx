745d5fe86b33e27ce4421869962e5c7a66c9bb9d
pione30 <g.pionelish30@gmail.com>
2020-11-11 00:40:30 +0900

    unify imports from useApi

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useApi } from "@/hooks/useApi";
-import type { UseApiOptions } from "@/hooks/useApi"; // eslint-disable-line no-unused-vars
+import { useApi, UseApiOptions } from "@/hooks/useApi"; // eslint-disable-line no-unused-vars
 import { getAuth0Audience } from "@/environmentVariables";
 import LoginButton from "./LoginButton";
 
