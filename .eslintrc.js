cf99378ab3dd2eec151cdf6f61239d5a29ba9675
pione30 <g.pionelish30@gmail.com>
2020-11-16 00:32:47 +0900

    plugin:@typescript-eslint/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
     "prettier/react"
