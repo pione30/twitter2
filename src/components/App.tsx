b5bfe13fb142c9e20279705240a85bf244671bcc
pione30 <g.pionelish30@gmail.com>
2020-11-16 00:32:00 +0900

    rename Profile to Posts, and show user's own posts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
-import Profile from "./Profile";
+import Posts from "./Posts";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
@@ -13,7 +13,7 @@ export const App = () => {
     <div>
       {isAuthenticated ? (
         <>
-          <Profile></Profile>
+          <Posts></Posts>
           <LogoutButton></LogoutButton>
         </>
       ) : (
