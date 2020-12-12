2f2be2c9572386489b47b9a3b4b49a3ca808cfd3
pione30 <g.pionelish30@gmail.com>
2020-12-13 00:06:38 +0900

    npx react-codemod update-react-imports
    https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html#removing-unused-react-imports

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useApi, UseApiOptions } from "@/hooks/useApi"; // eslint-disable-line no-unused-vars
 import { formatDate } from "@/helpers";
