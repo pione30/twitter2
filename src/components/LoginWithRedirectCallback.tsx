52fa5a59f5c18350dbe07abc345ccc4e4004d862
pione30 <g.pionelish30@gmail.com>
2021-01-18 01:43:24 +0900

    add LoginWithRedirectCallback to create user after loginWithRedirect

---
+++ src/components/LoginWithRedirectCallback.tsx
@@ -0,0 +1,27 @@
+import { useApi } from "@/hooks/useApi";
+import { ApiResultHandler } from "@/components/helpers/ApiResultHandler";
+import Posts from "./Posts";
+import LogoutButton from "./LogoutButton";
+
+export const LoginWithRedirectCallback = () => {
+  const apiOptions = {
+    scope: "write:users",
+    fetchOptions: {
+      method: "POST",
+    },
+  };
+
+  const { loading, error, refresh } = useApi("/users", apiOptions);
+
+  return (
+    <ApiResultHandler
+      loading={loading}
+      error={error}
+      refresh={refresh}
+      scope={apiOptions.scope}
+    >
+      <Posts></Posts>
+      <LogoutButton></LogoutButton>
+    </ApiResultHandler>
+  );
+};
